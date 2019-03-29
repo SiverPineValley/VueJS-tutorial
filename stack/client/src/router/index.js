0c50e96b39eb2b659ce36887c1713fcebf110fea
Jong in Park <whddls9632@naver.com>
2019-03-29 10:48:47 +0900

    server stack

--- stack/client/src/router/index.js
+++ stack/client/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Hello from '@/components/Hello'
+import Posts from '@/components/Posts'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+            path: '/',
+            name: 'Hello',
+            component: Hello
+        },
+        {
+            path: '/posts',
+            name: 'Posts',
+            component: Posts
+        }
+    ]
 })
