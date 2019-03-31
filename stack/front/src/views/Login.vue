5ffa03274cc35208e3ce0dad4ee38a1401ed955b
Jong in Park <whddls9632@naver.com>
2019-03-31 16:06:41 +0900

    Login Page

---
+++ stack/front/src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <LoginForm />
+</template>
+
+<script>
+    import LoginForm from '../components/Login/LoginForm.vue'
+
+  export default {
+    components: {
+      LoginForm
+    }
+  }
+</script>
