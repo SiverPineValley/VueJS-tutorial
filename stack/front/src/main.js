4f0d3e26caf43d13cad5e1403d50e1977a47b138
Jong in Park <whddls9632@naver.com>
2019-03-31 14:48:51 +0900

    add vuetify

--- stack/front/src/main.js
+++ stack/front/src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
+import Vuetify from 'vuetify'
+// index.js or main.js
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
-//fdfsdfes
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: h => h(App)
+}).$mount('#app');
